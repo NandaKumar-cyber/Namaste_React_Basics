6b23cbf4495774de42486770f24f554b53db3b4f
Nanda Kumar <65768970+NandaKumar-cyber@users.noreply.github.com>
2023-01-04 20:58:32 +0530

    CDN Added Changes

---
+++ App.js
@@ -0,0 +1,19 @@
+const heading1 = React.createElement(
+    "h1",{
+        id:"title",
+    },
+    "Heading 1"
+);
+
+const container = React.createElement(
+    "div",
+    {
+        id:"conatiner"
+    },
+    [heading1]
+)
+
+console.log(heading1)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
